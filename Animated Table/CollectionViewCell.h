0c5c486fd4621b8fe90fca901c2d294b26ecef37
sinmonli <sinmonli@sinmonlideMacBook-Pro.local>
2016-03-11 20:23:00 +0800

    1

---
+++ Animated Table/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  Animated Table
+//
+//  Created by sinmonli on 16/3/11.
+//  Copyright © 2016年 Philip Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Animated Table/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  Animated Table
+//
+//  Created by sinmonli on 16/3/11.
+//  Copyright © 2016年 Philip Yu. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
